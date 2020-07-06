170d38d7cd89c70fa65b7f7346549c232310dac0
ColinAllmanGame <colinallmangame@gmail.com>
2020-07-05 20:16:33 -0500

    data placeholders

---
+++ Burden System/Assets/System tools/WorldStateSaveData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WorldStateParmObject", menuName = "ScriptableObjects/MakeWorldStateObject", order = 1)]
+public class WorldStateSaveData : ScriptableObject
+{
+    public int worldFlag = 0;
+}
+
+[CreateAssetMenu(fileName = "StoryStateParmObject", menuName = "ScriptableObjects/MakeWorldStateObject", order = 1)]
+public class StoryStateSaveData : ScriptableObject
+{
+    public int StoryFlag = 0;
+}
+
+[CreateAssetMenu(fileName = "PlayerStateParmObject", menuName = "ScriptableObjects/MakeWorldStateObject", order = 1)]
+public class PlayerStateSaveData : ScriptableObject
+{
+    public int playerFlag = 0;
+}
